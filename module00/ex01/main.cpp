5f194134779e1fa754e49a3de1dc4cc71bb10cc2
kbarcelos.extreme <kbarcelos.extreme@mprj.mp.br>
2025-07-20 19:08:19 -0300

    ex01: esta tudo funcionando

---
+++ module00/ex01/main.cpp
@@ -0,0 +1,29 @@
+#include "phonebook.hpp"
+#include "phonebook_utils.hpp"
+#include <iostream>
+
+int main()
+{
+	Phonebook phonebook;
+
+	std::cout << "Iniciando o teste...";
+	std::cout << "Command info: " << std::endl;
+	std::cout << " -ADD: add a contact to the phonebook" << std::endl;
+	std::cout << " -SEARCH: search for a contact in the phonebook" << std::endl;
+	std::cout << " -EXIT: quit the program" << std::endl;
+	std::cout << " -DEBUGGER: Socorro!" << std::endl;
+
+	while(42)
+	{
+		std::string command;
+		std::cout << std::endl << "Enter a command: ";
+		std::getline(std::cin, command);
+
+		if(command == "ADD")
+			addContact(&phonebook);
+		else if(command == "SEARCH")
+			searchContact(&phonebook);
+		else if(command == "EXIT" || command == "")
+			break;
+	}
+}
---
+++ module00/ex01/phonebook_utils.hpp
@@ -0,0 +1,9 @@
+#ifndef PHONEBOOK_UTILS_HPP
+# define PHONEBOOK_UTILS_HPP
+
+class Phonebook;
+
+void    addContact(Phonebook *phonebook);
+void    searchContact(Phonebook *phonebook);
+
+#endif
