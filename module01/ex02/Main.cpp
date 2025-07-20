3d1e5c10c1bd381d09f78ca98d0f677df57ebfc3
kbarcelos.extreme <kbarcelos.extreme@mprj.mp.br>
2025-07-20 19:49:30 -0300

    ex:02 feito e finalizado

---
+++ module01/ex02/Main.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <iostream>
+
+/** 
+ * Declares a string = HI THIS IS BRAIN
+ * Creates a pointer to the String (stringPTR)
+ * Creates a reference to the string (stringREF)
+ * Shows the memory addresses and values associated with each one
+ */
+
+int main() {
+    std::string string = "HI THIS IS BRAIN";
+    std::string *stringPTR = &string;
+    std::string &stringREF = string;
+
+    std::cout << "String address: " << &string << std::endl;
+    std::cout << "Address stored in stringPTR pointer: " << stringPTR << std::endl;
+    std::cout << "Address of stringREF reference: " << &stringREF << std::endl;
+    std::cout << "------------------------------------------" << std::endl;
+    std::cout << "String value: " << string << std::endl;
+    std::cout << "Value of pointer dereference: " << *stringPTR << std::endl;
+    std::cout << "Value through stringREF reference: " << stringREF << std::endl;
+}
