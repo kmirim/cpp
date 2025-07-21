e3a2779ccadcbdf8ca17d5fdfa068d54a3a674f2
kmirim <kaluanabarcelos@gmail.com>
2025-07-21 17:37:23 -0300

    feito e testado

---
+++ module01/ex05/Harl.hpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+class Harl {
+    private:
+        void debug(void);
+        void info(void);
+        void warning(void);
+        void error(void);
+    public:
+        void complain(std::string level);
+};
---
+++ module01/ex05/main.cpp
@@ -0,0 +1,9 @@
+#include "Harl.hpp"
+
+int main(){
+    Harl testclass;
+    testclass.complain("DEBUG");
+    testclass.complain("INFO");
+    testclass.complain("WARNING");
+    testclass.complain("ERROR");
+}
