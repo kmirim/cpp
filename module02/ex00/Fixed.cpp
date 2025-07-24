3690ab26758155532113ad7b7d8507c1a7665c1b
kmirim <kaluanabarcelos@gmail.com>
2025-07-24 17:30:24 -0300

    tudo funcionando! retirar comentarios

--- module02/ex00/Fixed.cpp
+++ module02/ex00/Fixed.cpp
@@ -1,37 +1,38 @@
 #include "Fixed.hpp"
 
 //construtor parametrizado! foi indicado pelo subject que fpn deveria ter default 0
-Fixed::Fixed() : _fpn(0){
+Fixed::Fixed() : _fpn(0) { 
     std::cout << "Default constructor called" <<std::endl; 
 }
 
 //construtor cópia
-Fixed::Fixed(const Fixed &other) : _fpn(other._fpn){
-    std::cout << "Copy constructor called" << std::endl;       
+Fixed::Fixed(const Fixed &other) {
+    std::cout << "Copy constructor called" << std::endl; 
+    *this =other;
 }
 
-
-//
+//criar copia
 Fixed &Fixed::operator=(const Fixed &other)
 {
     std::cout << "Copy assignment operator called" << std::endl;
-    if(this != &other)
-        this->_fpn = other._fpn;
+    if(this == &other)
+        return *this;
+    this-> setRawBits(other.getRawBits());
     return *this;
 }
 
 //chamada do destrutor
-Fixed::~Fixed(){
-    std::cout << "Destructor called" << std::endl;
+Fixed::~Fixed(){ 
+    std::cout << "Destructor called" << std::endl; 
 }
 
+//instancia a funcao de ler os bits da memoria raw
 int Fixed::getRawBits(void) const{
     std::cout << "getRawBits member function called" << std::endl;
     return(_fpn);
 }
 
-
-void Fixed::setRawBits(int const raw){
-    std::cout << "" << std::endl;
-    _fpn = raw;
+//instancia o modificador da variavel raw
+void Fixed::setRawBits(int const raw){ 
+    _fpn = raw; 
 }
--- module02/ex00/main.cpp
+++ module02/ex00/main.cpp
@@ -1,14 +1,29 @@
 #include <iostream>
 #include "Fixed.hpp"
 int main (void){
-    Fixed a;
-    Fixed b (a);
-    Fixed c;
+    Fixed a; // construtor
+
+    std::cout << "-----------------" << std::endl;
+
+    Fixed b (a); // copia
+    
+    std::cout << "-----------------" << std::endl;
+    
+    Fixed c; // construtor
+    
+    std::cout << "-----------------" << std::endl;
+    
+
 
     c = b;
-
+    std::cout << "-----------------" << std::endl;
+    
     std::cout << a.getRawBits() << std::endl;
+    std::cout << "-----------------" << std::endl;
+    
     std::cout << b.getRawBits() << std::endl;
+    std::cout << "-----------------" << std::endl;
+    
     std::cout << c.getRawBits() << std::endl;
 
     return (0);
