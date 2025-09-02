4ec23c93acf91a78499fa800b66c843faa59ba57
kmirim <kaluanamba@gmail.com>
2025-09-02 20:36:19 -0300

    feito

--- module02/ex01/fixed.cpp
+++ module02/ex01/fixed.cpp
@@ -1,38 +1,55 @@
-#include "Fixed.hpp"
+#include "fixed.hpp"
 
-//construtor parametrizado! foi indicado pelo subject que fpn deveria ter default 0
+Fixed::Fixed(const float num_float) 
+    : _fpn(num_float * float (1 << _nfb) + (num_float >= 0 ? 0.5 : -0.5)) {
+        std::cout << "Float constructor called\n" << std::endl;
+}
+
+Fixed::Fixed(const int num_int) 
+    : _fpn(num_int * int(1 << _nfb) + (num_int >= 0 ? 0.5 : -0.5)) 
+{
+    std::cout << "Int constructor called\n" << std::endl;
+}
+
 Fixed::Fixed() : _fpn(0) { 
     std::cout << "Default constructor called" <<std::endl; 
 }
 
-//construtor cópia
 Fixed::Fixed(const Fixed &other) {
     std::cout << "Copy constructor called" << std::endl; 
     *this =other;
 }
 
-//criar copia
 Fixed &Fixed::operator=(const Fixed &other)
 {
     std::cout << "Copy assignment operator called" << std::endl;
     if(this == &other)
         return *this;
-    this-> setRawBits(other.getRawBits());
     return *this;
 }
 
-//chamada do destrutor
 Fixed::~Fixed(){ 
     std::cout << "Destructor called" << std::endl; 
 }
 
-//instancia a funcao de ler os bits da memoria raw
 int Fixed::getRawBits(void) const{
     std::cout << "getRawBits member function called" << std::endl;
     return(_fpn);
 }
 
-//instancia o modificador da variavel raw
 void Fixed::setRawBits(int const raw){ 
     _fpn = raw; 
 }
+
+float Fixed::toFloat(void) const{
+    return(float(_fpn) / float(1<<_nfb));
+}
+
+int Fixed::toInt(void) const{ 
+    return (int(_fpn) / int(1 << _nfb));
+}
+
+std::ostream &operator<<(std::ostream &out, const Fixed &right) {
+    out << right.toFloat();
+    return (out);
+};
--- module02/ex02/main.cpp
+++ module02/ex02/main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include "Fixed.hpp"
+#include "fixed.hpp"
 int main(void)
 {
     Fixed a;
