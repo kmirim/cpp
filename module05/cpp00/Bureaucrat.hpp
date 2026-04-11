93f3df7388df5c266f576dab5b731fcc1cb749df
Colaborador <kbarcelos.extreme@mprj.mp.br>
2026-04-11 19:04:34 -0300

    feat: ex00

--- module05/cpp00/Bureaucrat.hpp
+++ module05/cpp00/Bureaucrat.hpp
@@ -1,5 +1,6 @@
 #ifndef BUREAUCRAT_HPP
 # define BUREAUCRAT_HPP
+
 # include <iostream>
 # include <string> 
 # include <exception>
@@ -10,34 +11,34 @@ class Bureaucrat{
         int                 _grade;
 
     public: 
-        Bureaucrat(); //construtor padrao
-        Bureaucrat(std::string const name, int grade); // construtor com parametro
-        Bureaucrat(const Bureaucrat &other); // construtor de copia
-        Bureaucrat &operator=(const Bureaucrat &rhs); // overload de operador
-        ~Bureaucrat(); // destrutor  
+        Bureaucrat();
+        Bureaucrat(std::string const name, int grade);
+        Bureaucrat(const Bureaucrat &other);
+        Bureaucrat &operator=(const Bureaucrat &rhs);
+        ~Bureaucrat(); 
 
         
-        std::string getName() const; // const ao final indica que o metodo nao altera a classe
+        std::string getName() const;
         int         getGrade() const;
 
         void incrementGrade();
         void decrementGrade();
 
-
         class GradeTooHighException : public std::exception{
             public:
-                vitual const char *what() const throw() {
+                virtual const char *what() const throw() {
                     return ("Grade is too high!");
                 }
         };
 
         class GradeTooLowException : public std::exception{
-            public const char *what() const throw(){
-                return ("Grade is too low!");
-            }
+            public:
+                const char *what() const throw(){
+                    return ("Grade is too low!");
+                }
         };
 };
 
-std::ostream &operator<<(std::ostream &o, Bureucrat const &rhs);
+std::ostream &operator<<(std::ostream &o, Bureaucrat const &rhs);
 
 #endif 
--- module05/cpp00/main.cpp
+++ module05/cpp00/main.cpp
@@ -1,5 +1,46 @@
 #include "Bureaucrat.hpp"
 
-int main(){
-    
+int main()
+{
+    try 
+    {  
+        std::cout << "--- Default case: ---" << std::endl;
+        Bureaucrat b1("Kaluanã", 2);
+        std::cout << b1 << std::endl;
+        
+        b1.incrementGrade();
+        std::cout << "After increment: " << b1 << std::endl;
+    }
+    catch (std::exception &e) 
+    {
+        std::cerr << "Erro: " << e.what() << std::endl;
+    }
+
+    std::cout << std::endl;
+
+    try 
+    {
+        std::cout << "--- Grade Too High ---" << std::endl;
+        Bureaucrat b2("Chefe", 1);
+        std::cout << b2 << std::endl;
+        b2.incrementGrade();
+    }
+    catch (std::exception &e) 
+    {
+        std::cout << "Catch: " << e.what() << std::endl; // 
+    }
+
+    std::cout << std::endl;
+
+    try 
+    {
+        std::cout << "--- Construtor Invalido (151) ---" << std::endl;
+        Bureaucrat b3("Estagiario", 151);
+    }
+    catch (std::exception &e) 
+    {
+        std::cout << "Catch: " << e.what() << std::endl;
+    }
+
+    return 0;
 }
